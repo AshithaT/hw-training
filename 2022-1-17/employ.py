fe7245da3e02c84bbb398123d75e0361a030917f
Ashitha <ashithasl1996@gmail.com>
2022-01-19 17:45:54 +0530

    database created

--- 2022-1-17/employ.py
+++ 2022-1-17/employ.py
@@ -1,9 +1,18 @@
 from datetime import datetime
 import json
+import pymongo
+from pymongo import MongoClient
+import pprint
 
+client = MongoClient() 
+db=client.employee
+result = db.emp.create_index([('emp_id', pymongo.ASCENDING)],
+                                  unique=True)
 task1=[]
 
 class employ():
+    
+
     def __init__(self, emp_id, emp_name):
         self.emp_id=emp_id
         self.emp_name=emp_name
@@ -27,14 +36,27 @@ class employ():
         return task1
     
     def logout(self):
+        
         details = {
             'name': self.emp_name,
             'emp_id':self.emp_id,
             'login_time': self.login_time,
             'logout_time':datetime.now().strftime("%Y-%m-%d %H:%M"),
             'task': task1
+
         }
-        details=json.dumps(details)
-        print(details)
-        with open(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+self.emp_name+'.json',"w") as fp:
-           fp.write(json.dumps(json.loads(details),indent=4))
+        try:
+            post=db.emp.insert_one(details)
+        except : 
+            print("Duplicate key error")
+            
+        
+    def show_task(self,emp_id):
+        
+        pprint.pprint(db.emp.find_one({"emp_id": emp_id}))  
+       
+        
+        # details=json.dumps(details)
+        # print(details)
+        # with open(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+self.emp_name+'.json',"w") as fp:
+        #    fp.write(json.dumps(json.loads(details),indent=4))
