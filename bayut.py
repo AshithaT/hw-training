2bae36174cd9fac88b3c4114aef7d3b20148acca
Ashitha <ashithasl1996@gmail.com>
2022-01-20 17:49:27 +0530

    python requests sampling

--- bayut.py
+++ bayut.py
@@ -10,6 +10,6 @@ headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTM
 page = requests.get('https://www.bayut.com/to-rent/property/dubai/',headers=headers)
 tree = html.fromstring(page.content)
 print(page.text)
-hotel_name = tree.xpath('//div[@class="_4041eb80"]/text()')
+hotel_name = tree.xpath('//div[@class="e676c252"]/text()')
 
 print(hotel_name)
